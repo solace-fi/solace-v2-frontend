eeae8ed28edaac43a86c770fe896c9108e8855c0
DannyFung97 <dannyfung97@gmail.com>
2023-05-18 13:27:25 -0700

    fix: explorer type change accomodation

--- src/utils/explorer.ts
+++ src/utils/explorer.ts
@@ -11,16 +11,19 @@ export const getExplorerItemUrl = (
 export async function fetchGasPrice(
   activeNetwork: Network
 ): Promise<GasPriceResult> {
-  const key = activeNetwork.explorer.key
-    ? { apikey: activeNetwork.explorer.key }
+  const key = activeNetwork.local.explorer.key
+    ? { apikey: activeNetwork.local.explorer.key }
     : {}
-  const { data } = await axios.get(`${activeNetwork.explorer.apiUrl}/api`, {
-    params: {
-      module: 'gastracker',
-      action: 'gasoracle',
-      ...key,
-    },
-  })
+  const { data } = await axios.get(
+    `${activeNetwork.local.explorer.apiUrl}/api`,
+    {
+      params: {
+        module: 'gastracker',
+        action: 'gasoracle',
+        ...key,
+      },
+    }
+  )
   const result = data.result
   const fetchedResult: GasPriceResult = {
     fast: Number(result.FastGasPrice),
@@ -36,22 +39,25 @@ export async function fetchExplorerTxHistoryByAddress(
   activeNetwork: Network,
   address: string
 ): Promise<any> {
-  const key = activeNetwork.explorer.key
-    ? { apikey: activeNetwork.explorer.key }
+  const key = activeNetwork.local.explorer.key
+    ? { apikey: activeNetwork.local.explorer.key }
     : {}
-  const { data } = await axios.get(`${activeNetwork.explorer.apiUrl}/api`, {
-    params: {
-      module: 'account',
-      action: 'txlist',
-      address,
-      startblock: '0',
-      endblock: 'latest',
-      page: '1',
-      offset: '4500',
-      sort: 'desc',
-      ...key,
-    },
-  })
+  const { data } = await axios.get(
+    `${activeNetwork.local.explorer.apiUrl}/api`,
+    {
+      params: {
+        module: 'account',
+        action: 'txlist',
+        address,
+        startblock: '0',
+        endblock: 'latest',
+        page: '1',
+        offset: '4500',
+        sort: 'desc',
+        ...key,
+      },
+    }
+  )
   return data
 }
 
@@ -59,19 +65,22 @@ export async function fetchTransferEventsOfUser(
   activeNetwork: Network,
   user: string
 ): Promise<any> {
-  const key = activeNetwork.explorer.key
-    ? { apikey: activeNetwork.explorer.key }
+  const key = activeNetwork.local.explorer.key
+    ? { apikey: activeNetwork.local.explorer.key }
     : {}
-  const { data } = await axios.get(`${activeNetwork.explorer.apiUrl}/api`, {
-    params: {
-      module: 'account',
-      action: 'tokentx',
-      address: `${user}`,
-      startblock: '0',
-      endblock: 'latest',
-      ...key,
-    },
-  })
+  const { data } = await axios.get(
+    `${activeNetwork.local.explorer.apiUrl}/api`,
+    {
+      params: {
+        module: 'account',
+        action: 'tokentx',
+        address: `${user}`,
+        startblock: '0',
+        endblock: 'latest',
+        ...key,
+      },
+    }
+  )
   const result = data.result
   if (result != 'Max rate limit reached') return result
   return []
